e400741511fc35feb8bcb8ceb84441d874dfeace
FAURE Benjamin <benjamin.faure@inist.fr>
2017-11-28 10:11:23 +0100

    SenyuAPI pointe sur les données du serveur

--- src/api/SenyuAPI.js
+++ src/api/SenyuAPI.js
@@ -1,51 +1,92 @@
-import 'whatwg-fetch';
+import axios from 'axios';
 import 'babel-polyfill';
 
+const API_URL = 'http://senyu.fr:9876/ws';
+const API_HEADERS = {
+  'Content-Type': 'application/json;charset=UTF-8',
+}
 
 let SenyuAPI = {
 
-  fetchIntervenants() {
+  async fetchIntervenants() {
+    try {
+      const response = await axios.get(`${API_URL}/invites`, { headers: API_HEADERS });
+      return response.data;
+    } catch (err) {
+      console.error(`Un erreur s'est produire lors de la réception des invités : ${err.message}`)
+    }
+  },
+  async fetchIntervenantById(id) {
 
-    return fetch('/intervenants.json')
-    .then((response) => response.json());
-  },
-  fetchIntervenantById(id) {
-    return fetch('/intervenants.json')
-    .then((response) => response.json())
-    .then((response) => response[id - 1]);
-  },
-
-  fetchFAQ() {
-    return fetch('/faq.json')
-    .then((response) => response.json());
-  },
-
-
-  fetchExposants() {
-    return fetch('/exposants.json')
-    .then((response) => response.json());
-  },
-  fetchExposantsByType(type) {
-    return fetch('/exposants.json')
-    .then((response) => response.json())
-    .then((response) => response.filter(exposant => exposant.type === type));
-  },
-  fetchExposantById(id) {
-    return fetch('/exposants.json')
-    .then((response) => response.json())
-    .then((response) => response[id - 1]);
+    try {
+      const response = await axios.get(`${API_URL}/invites/${id}`, { headers: API_HEADERS });
+      return response.data;
+    } catch (err) {
+      console.error(`Un erreur s'est produire lors de la réception d'un invité' : ${err.message}`)
+    }
   },
 
-
-  fetchSenyuAnimations() {
-
-    return fetch('/animationsSenyu.json')
-    .then((response) => response.json());
+  async fetchFAQ() {
+    try {
+      const response = await axios.get(`${API_URL}/faq`, { headers: API_HEADERS });
+      return response.data;
+    } catch (err) {
+      console.error(`Un erreur s'est produire lors de la réception de la faq' : ${err.message}`)
+    }
   },
-  fetchSenyuAnimationById(id) {
-    return fetch('/animationsSenyu.json')
-    .then((response) => response.json())
-    .then((response) => response[id - 1]);
+
+  async fetchExposants() {
+    try {
+      const response = await axios.get(`${API_URL}/exposants`, { headers: API_HEADERS });
+      return response.data;
+    } catch (err) {
+      console.error(`Un erreur s'est produire lors de la réception des exposants : ${err.message}`)
+    }
+  },
+
+  async fetchExposantsByType(type) {
+    try {
+      const response = await axios.get(`${API_URL}/exposants`, { headers: API_HEADERS });
+      return response.data.filter(exposant => exposant.type === type);
+    } catch (err) {
+      console.error(`Un erreur s'est produire lors de la réception des exposants : ${err.message}`)
+    }
+  },
+
+  async fetchExposantById(id) {
+    try {
+      const response = await axios.get(`${API_URL}/exposants/${id}`, { headers: API_HEADERS });
+      return response.data;
+    } catch (err) {
+      console.error(`Un erreur s'est produire lors de la réception d'un exposant' : ${err.message}`)
+    }
+  },
+
+  async fetchSenyuAnimations() {
+    try {
+      const response = await axios.get(`${API_URL}/animations`, { headers: API_HEADERS });
+      return response.data;
+    } catch (err) {
+      console.error(`Un erreur s'est produire lors de la réception des animations : ${err.message}`)
+    }
+  },
+
+  async fetchSenyuAnimationsByType(type){
+    try {
+      const response = await axios.get(`${API_URL}/animations`, { headers: API_HEADERS });
+      return response.data.filter(animation => animation.type === type);
+    } catch (err) {
+      console.error(`Un erreur s'est produire lors de la réception des animations : ${err.message}`)
+    }
+  },
+
+  async fetchSenyuAnimationById(id) {
+    try {
+      const response = await axios.get(`${API_URL}/animations/${id}`, { headers: API_HEADERS });
+      return response.data;
+    } catch (err) {
+      console.error(`Un erreur s'est produire lors de la réception d'une animation' : ${err.message}`)
+    }
   },
 
 };
