115619d775643712b7290f2411851056129267d5
FAURE Benjamin <benjamin.faure@inist.fr>
2018-02-05 16:47:51 +0100

    Correction class en className

--- src/components/Cosplay/Cosplay.jsx
+++ src/components/Cosplay/Cosplay.jsx
@@ -18,7 +18,7 @@ class Cosplay extends Component {
       <div>
         {meta}
         <GenericPDFDisplay filePath="/cosplay.pdf" />
-        <p class="formulaire-inscription-cosplay">
+        <p className="formulaire-inscription-cosplay">
           <a href="https://docs.google.com/forms/d/e/1FAIpQLSdvUVuJPq5twxyaisPLWRXXM677IhBbPgBNaS4f67dcP9utQA/viewform">Formulaire d'inscription</a>
         </p>
       </div>
