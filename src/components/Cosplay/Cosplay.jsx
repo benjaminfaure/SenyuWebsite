b2a2cc65011c6627f31f537f9ee90c6d5347a703
FAURE Benjamin <benjamin.faure@inist.fr>
2018-02-05 08:19:24 +0100

    Ajout lien Cosplay

--- src/components/Cosplay/Cosplay.jsx
+++ src/components/Cosplay/Cosplay.jsx
@@ -18,6 +18,9 @@ class Cosplay extends Component {
       <div>
         {meta}
         <GenericPDFDisplay filePath="/cosplay.pdf" />
+        <p class="formulaire-inscription-cosplay">
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSdvUVuJPq5twxyaisPLWRXXM677IhBbPgBNaS4f67dcP9utQA/viewform">Formulaire d'inscription</a>
+        </p>
       </div>
     );
   }
