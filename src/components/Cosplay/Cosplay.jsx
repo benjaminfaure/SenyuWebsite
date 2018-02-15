6ad0b541ac72bda656c29f98ec152be3092ed21e
Benjamin FAURE <benjamin.faure@inist.fr>
2018-02-15 21:21:05 +0100

    Ajout texte dans Cosplay

--- src/components/Cosplay/Cosplay.jsx
+++ src/components/Cosplay/Cosplay.jsx
@@ -21,6 +21,9 @@ class Cosplay extends Component {
           <h1 className="generic-page-title">Règlement Cosplay</h1>
         </section>
         <GenericPDFDisplay filePath="/cosplay.pdf" />
+        <p className="info-inscription-cosplay">
+          L'inscription au concours octroie une réduction sur le billet d'entrée qui correspond à 1,5€ de remise. La réduction s'applique sur le billet les  jours de participation au concours cosplay. Ainsi, si vous participez aux deux jours du concours, vous pourrez bénéficier jusqu'à 3€ de remise. Vous aurez également la possibilité d'avoir un coupe-fil en accédant directement à la convention depuis l'entrée sur le parking P4. Une vérification sera effectuée par cette entrée. Cette réduction s'appliquera par une remise lors de la validation de votre inscription au concours cosplay, dans la zone dédiée au Cosplay.
+        </p>
         <p className="formulaire-inscription-cosplay">
           <a href="https://docs.google.com/forms/d/e/1FAIpQLSdvUVuJPq5twxyaisPLWRXXM677IhBbPgBNaS4f67dcP9utQA/viewform" target="_blank" rel="noopener noreferrer">Formulaire d'inscription</a>
         </p>
