d0334ea39a6dfe54d80468a64d905acedf941f70
FAURE Benjamin <benjamin.faure@inist.fr>
2018-04-10 14:12:43 +0200

    Déport des routes vers un fichier à part

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,9 @@ import { Helmet } from "react-helmet";
 import PropTypes from 'prop-types';
 import ScrollToTop from 'react-scroll-up';
 import FaCircleOArrowUp from 'react-icons/lib/fa/arrow-circle-o-up';
-import { Route, Switch } from 'react-router-dom';
 import { withRouter } from 'react-router'
 import { connect } from 'react-redux';
+import { renderRoutes } from 'react-router-config';
 
 
 import { WIDTH_BREAKPOINT } from '../constants';
@@ -14,51 +14,13 @@ import {
   getDimensions,
 } from '../reducers';
 
+import thunk from 'redux-thunk';
+
+
 import Menu from './Menu/Menu.jsx';
 import Header from './Header/Header.jsx';
 import Footer from './Footer/Footer.jsx';
 
-import MainPage from './MainPage/MainPage.jsx';
-
-import Intervenant from './Intervenants/Intervenant.jsx';
-import Intervenants from './Intervenants/Intervenants.jsx';
-
-import Animations from './Animations/Animations.jsx';
-import Animation from './Animations/Animation.jsx';
-
-import Exposant from './Exposants/Exposant.jsx';
-import Exposants from './Exposants/Exposants.jsx';
-
-import FAQ from './FAQ/FAQ.jsx';
-
-import Stream from './Stream/Stream.jsx';
-
-import Cosplay from './Cosplay/Cosplay.jsx';
-
-import Communication from './Communication/Communication.jsx';
-
-import Reglements from './Reglements/Reglements.jsx';
-import Reglement from './Reglements/Reglement.jsx';
-
-import Contact from './Contact/Contact.jsx';
-
-import Recrutement from './Recrutement/Recrutement.jsx';
-
-import Billeterie from './Billeterie/Billeterie.jsx';
-
-import Planning from './Planning/Planning.jsx';
-
-import Plans from './Plans/Plans.jsx';
-
-import Partenaires from './Partenaires/Partenaires.jsx';
-
-import Guide from './Guide/Guide.jsx';
-
-import EnTravaux from './EnTravaux/EnTravaux.jsx';
-
-import Redirect from './Redirect/Redirect.jsx';
-
-import NotFound from './NotFound/NotFound.jsx';
 
 import './App.css';
 
@@ -138,53 +100,8 @@ class App extends Component {
         <Menu currentLocation={this.state.currentLocation}/>
         <main className={this.state.isMenuOpen ? "main-content open" : "main-content"}>
           {errorMessage}
-          <Switch>
-            <Route exact path="/" component={MainPage} />
-            {/* Intervenants */}
-            <Route exact path="/invites" component={Intervenants} />
-            <Route path="/invites/:intervenantId" component={Intervenant} />
-            {/* Exposants */}
-            <Route exact path="/exposants" component={Exposants} />
-            <Route path="/exposants/:exposantId" component={Exposant} />
-            {/* Animations */}
-            <Route exact path="/animations" component={Animations} />
-            <Route path="/animations/:animationId" component={Animation} />
-            {/* FAQ */}
-            <Route exact path="/faq" component={FAQ} />
-            {/* Reglements */}
-            <Route exact path="/reglements" component={Reglements} />
-            <Route path="/reglements/:reglementType" component={Reglement} />
-            {/* Plans */}
-            <Route exact path="/plans" component={Plans} />
-            {/* Planning */}
-            <Route exact path="/planning" component={Planning} />
-            {/* Guide */}
-            <Route exact path="/guide" component={Guide} />
-            {/* Contact */}
-            <Route exact path="/contact" component={Contact} />
-            {/* Cosplay */}
-            <Route exact path="/cosplay" component={Cosplay} />
-            {/* Communication */}
-            <Route exact path="/communication" component={Communication} />
-            {/* Stream */}
-            <Route exact path="/stream" component={Stream} />
-            {/* Presse */}
-            <Route exact path="/presse" component={EnTravaux} />
-            {/* Archive */}
-            <Route exact path="/archive" component={EnTravaux} />
-            {/* Partenaires */}
-            <Route exact path="/partenaires" component={Partenaires} />
-            {/* Recrutement */}
-            <Route exact path="/recrutement" component={Recrutement} />
-            {/* Billeterie */}
-            <Route exact path="/billeterie" component={EnTravaux} />
-            {/* Redirection Inscriptions */}
-            <Route exact path="/ws/inscriptions" component={Redirect} />
-            <Route path="/ws/inscriptions/:typeInscription" component={Redirect} />
 
-            {/* 404 */}
-            <Route component={NotFound} />
-          </Switch>
+          {renderRoutes(this.props.route.routes)}
         </main>
         <Footer />
       </div>
---
+++ src/routes.js
@@ -0,0 +1,188 @@
+import App from './components/App.jsx';
+
+import MainPage from './components/MainPage/MainPage.jsx';
+
+import Intervenant from './components/Intervenants/Intervenant.jsx';
+import Intervenants from './components/Intervenants/Intervenants.jsx';
+
+import Animations from './components/Animations/Animations.jsx';
+import Animation from './components/Animations/Animation.jsx';
+
+import Exposant from './components/Exposants/Exposant.jsx';
+import Exposants from './components/Exposants/Exposants.jsx';
+
+import FAQ from './components/FAQ/FAQ.jsx';
+
+import Stream from './components/Stream/Stream.jsx';
+
+import Cosplay from './components/Cosplay/Cosplay.jsx';
+
+import Communication from './components/Communication/Communication.jsx';
+
+import Reglements from './components/Reglements/Reglements.jsx';
+import Reglement from './components/Reglements/Reglement.jsx';
+
+import Contact from './components/Contact/Contact.jsx';
+
+import Recrutement from './components/Recrutement/Recrutement.jsx';
+
+import Billeterie from './components/Billeterie/Billeterie.jsx';
+
+import Planning from './components/Planning/Planning.jsx';
+
+import Plans from './components/Plans/Plans.jsx';
+
+import Partenaires from './components/Partenaires/Partenaires.jsx';
+
+import Guide from './components/Guide/Guide.jsx';
+
+import EnTravaux from './components/EnTravaux/EnTravaux.jsx';
+
+import Redirect from './components/Redirect/Redirect.jsx';
+
+import NotFound from './components/NotFound/NotFound.jsx';
+
+
+
+
+const routes = [
+  {
+    component: App,
+    routes: [
+      {
+        path: '/',
+        exact: true,
+        component: MainPage
+      },
+      /* Intervenants */
+      {
+        path: '/invites',
+        exact: true,
+        component: Intervenants
+      },
+      {
+        path: '/invites/:intervenantId',
+        component: Intervenant
+      },
+      /* Exposants */
+      {
+        path: '/exposants',
+        exact: true,
+        component: Exposants
+      },
+      {
+        path: '/exposants/:exposantId',
+        component: Exposant
+      },
+      /* Animations */
+      {
+        path: '/animations',
+        exact: true,
+        component: Animations
+      },
+      {
+        path: '/animations/:animationId',
+        component: Animation
+      },
+      /* FAQ */
+      {
+        path: '/faq',
+        exact: true,
+        component: FAQ
+      },
+      /* Reglements */
+      {
+        path: '/reglements',
+        exact: true,
+        component: Reglements
+      },
+      {
+        path: '/reglements/:reglementType',
+        component: Reglement
+      },
+      /* Plans */
+      {
+        path: '/plans',
+        exact: true,
+        component: Plans
+      },
+      /* Planning */
+      {
+        path: '/planning',
+        exact: true,
+        component: Planning
+      },
+      /* Guide */
+      {
+        path: '/guide',
+        exact: true,
+        component: Guide
+      },
+      /* Contact */
+      {
+        path: '/contact',
+        exact: true,
+        component: Contact
+      },
+      /* Cosplay */
+      {
+        path: '/cosplay',
+        exact: true,
+        component: Cosplay
+      },
+      /* Communication */
+      {
+        path: '/communication',
+        exact: true,
+        component: Communication
+      },
+      /* Stream */
+      {
+        path: '/stream',
+        exact: true,
+        component: Stream
+      },
+      /* Archive */
+      {
+        path: '/archive',
+        exact: true,
+        component: EnTravaux
+      },
+      /* Partenaires */
+      {
+        path: '/partenaires',
+        exact: true,
+        component: Partenaires
+      },
+      /* Recrutement */
+      {
+        path: '/recrutement',
+        exact: true,
+        component: Recrutement
+      },
+      /* Billeterie */
+      {
+        path: '/billeterie',
+        exact: true,
+        component: EnTravaux
+      },
+      /* Redirection Inscriptions */
+      {
+        path: '//ws/inscriptions',
+        exact: true,
+        component: Redirect
+      },
+      {
+        path: '/ws/inscriptions/:typeInscription',
+        component: Redirect
+      },
+      /* 404 */
+      {
+        component: NotFound
+      },
+    ]
+  }
+];
+
+export default routes;
+
