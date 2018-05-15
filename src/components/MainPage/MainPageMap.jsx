0e7057d09ecf7e53a0c602062769c676ab0f73f3
FAURE Benjamin <benjamin.faure@inist.fr>
2018-05-15 15:42:13 +0200

    Fix pour production

--- src/components/MainPage/MainPageMap.jsx
+++ src/components/MainPage/MainPageMap.jsx
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
-
-
 import './MainPageMap.css';
 
-export default class SimpleExample extends Component {
-  state = {
-    lat: 48.1850019,
-    lng: 6.4693901,
-    zoom: 16,
+let Map, TileLayer, Marker, Popup;
+class MainPageMap extends Component {
+
+  componentDidMount(){
+    //Only runs on Client, not on server render
+    Map = require('react-leaflet').Map;
+    TileLayer = require('react-leaflet').TileLayer;
+    Marker = require('react-leaflet').Marker;
+    Popup = require('react-leaflet').Popup;
+    this.forceUpdate();
   }
 
   render() {
-    const position = [this.state.lat, this.state.lng]
+    const position = [48.1850019, 6.4693901]
     return (
-      <Map center={position} zoom={this.state.zoom} className="main-page-map">
+      (Map)
+      ?
+      (<Map center={position} zoom={16} className="main-page-map">
         <TileLayer
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -22,11 +26,15 @@ export default class SimpleExample extends Component {
         <Marker position={position}>
           <Popup>
             <span>
-              A pretty CSS3 popup. <br /> Easily customizable.
+              Centre des Congrés d'Epinal
             </span>
           </Popup>
         </Marker>
-      </Map>
+      </Map>)
+      : null
     )
   }
+
 }
+
+export default MainPageMap;
